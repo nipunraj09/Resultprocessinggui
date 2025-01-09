5c50a985a41e772cf90633db5964769f490ee57c
nipunraj09 <103949203+nipunraj09@users.noreply.github.com>
2025-01-09 17:31:55 +0530

    Create edit.py

---
+++ edit.py
@@ -0,0 +1,6 @@
+import sys
+import pandas as pd
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QVBoxLayout, QPushButton, QLabel, QFileDialog, QWidget, QMessageBox
+)
+from PyQt5.QtCore import Qt
